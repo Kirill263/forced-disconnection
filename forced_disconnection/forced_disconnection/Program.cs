4345022804a06cbdf0ab9cb15d5d5d18fd7f5734
Кирилл <myakiishevkirill@gmail.com>
2023-08-24 13:32:59 +1000

    added forced disconnection

---
+++ forced_disconnection/forced_disconnection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        string mode = "runas";
+        IntPtr result = ShellExecute(IntPtr.Zero, mode, "cmd.exe", "/K C:\\cports\\cports.exe /close 127.0.0.1 11000 * *", null, ShowCommands.SW_SHOWNORMAL);
+        Console.WriteLine("Соединение разорвано");
+
+        Environment.Exit(0);
+    }
+
+    [System.Runtime.InteropServices.DllImport("shell32.dll")] // Атрибут для импорта функции ShellExecute из библиотеки shell32.dll
+    private static extern IntPtr ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, ShowCommands nShowCmd);
+
+    private enum ShowCommands : int
+    {
+        SW_SHOWNORMAL = 1
+    }
+}
